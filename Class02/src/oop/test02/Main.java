6945493539a563fc1355fb4c3bb85ca79cb87f95
yseojin1@gmail.com <yseojin1@gmail.com>
2022-01-30 23:12:11 +0900

    Player 수정합니다.

--- Class02/src/oop/test02/Main.java
+++ Class02/src/oop/test02/Main.java
@@ -10,6 +10,30 @@ public class Main {
 		//[1]과 [2]를 생성하시고 정보를 출력한 뒤 서로 2번 공격하면 어떻게 되는지 구현
 		//attack()메소드를 Player에 추가한 다음,
 		//공격하면 상대방의 체력이 나의 공격력만큼 감소되도록 구현
+		
+		Player p1 = new Player("뽀로로",10,100);
+		Player p2 = new Player("타요",15,80);
+		
+		p1.setTarget(p2);
+		p2.setTarget(p1);
+		
+		p1.disp();
+		p2.disp();
+		
+		System.out.println("게임 시작!");
+		
+		
+		for (int i=0;i<2;i++) {
+			p1.attack();
+			p1.disp();
+			p2.disp();
+			
+			p2.attack();
+			p1.disp();
+			p2.disp();
+		}
+		
+		
 	}
 
 }
--- Class02/src/oop/test02/Player.java
+++ Class02/src/oop/test02/Player.java
@@ -6,29 +6,46 @@ public class Player {
 	//(구성항목)	아이디(id),공격력(atk),체력(hp),공격대상(target)
 	//atk과 hp는 양수만 설정되도록 구현( >0)
 	
-	private String id,target;
+	private String id;
 	private int atk,hp;
+	private Player target; //String target이 아님!
 	
 	
-	Player(String id, int atk, int hp){
+	public Player(String id, int atk, int hp){
 		this.id = id;
 		this.setAtk(atk);
 		this.setHp(hp);
 	}
 	
 	
+	//setter
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public void setAtk(int atk) {
+		if (atk < 0) {
+			System.out.println("공격력을 잘못 입력하셨습니다.");
+			this.atk = 1;
+			return;
+		}
 		this.atk = atk;
 	}
 	
 	public void setHp(int hp) {
+		if (hp < 0) {
+			System.out.println("체력을 잘못 입력하셨습니다.");
+			this.hp = 1;
+			return;
+		}
 		this.hp = hp;
 	}
 	
+	public void setTarget(Player target) { //String target이 아님!
+		this.target = target;
+	}
+	
+	//getter
 	public String getId() {
 		return id;
 	}
@@ -39,6 +56,20 @@ public class Player {
 	
 	public int getHp() {
 		return hp;
+	}
+	
+	public Player getTarget() {
+		return target;
+	}
+	
+	public void attack() {
+		target.hp -= this.atk; //한번공격:0=80-10; 60=70-10; 
+		//this.setTarget()할 필요 없음!
+	}
+	
+	
+	public void disp() {
+		System.out.println("아이디: "+id+"\t체력: "+hp+"\t\t공격력: "+atk+"\t상대방: "+target.getId());
 	}
 }
 
